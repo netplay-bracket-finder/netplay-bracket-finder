75296a029752d0d99d472431f1b86538b2ce290c
Jonathan Strickland <djanatyn@gmail.com>
2022-09-13 14:07:12 -0400

    fetch JSON and display tournament events

--- src/Main.elm
+++ src/Main.elm
@@ -3,13 +3,22 @@ module Main exposing (..)
 import Browser exposing (Document)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode as D
+import Json.Encode as E
+import Maybe exposing (withDefault)
+import Time exposing (Posix, toHour, toMinute, toSecond, utc)
 
 
+type Model
+    = Loading
+    | Failure
+    | Success (List TournamentEvent)
 
--- import Html.Events exposing (onInput, onClick)
--- import Http
--- import Json.Decode as D
--- import Json.Encode as E
+
+type Msg
+    = Loaded (Result Http.Error (List TournamentEvent))
 
 
 main =
@@ -21,39 +30,24 @@ main =
         }
 
 
-type Model
-    = Init (List TournamentEvent)
-
-
-type alias TournamentEvent =
-    { name : String
-    , image : String
-    , entrants : Int
-    , startTime : String
-    }
-
-
-placeHolder : TournamentEvent
-placeHolder =
-    { name = "Legs' Le Tournament #41"
-    , image = "https://images.smash.gg/images/tournament/460848/image-4f3871030bbb1e419191348fe7552839.png"
-    , entrants = 27
-    , startTime = "Sat Jul 16 5pm"
-    }
-
-
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Init (List.repeat 25 placeHolder), Cmd.none )
-
-
-type Msg
-    = Null
+    ( Loading
+    , Http.get
+        { url = "https://gist.githubusercontent.com/djanatyn/975e4473a487fe73d6931c2b5beb5bfc/raw/dea96f730630ab756027d9d4095a5c0eff6e9cd7/upcoming_events.json"
+        , expect = Http.expectJson Loaded (D.list eventDecoder)
+        }
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update Null model =
-    ( model, Cmd.none )
+update (Loaded result) model =
+    case result of
+        Ok events ->
+            ( Success events, Cmd.none )
+
+        Err _ ->
+            ( Failure, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -61,28 +55,67 @@ subscriptions model =
     Sub.none
 
 
+type alias TournamentEvent =
+    { slug : String
+    , tournament_name : String
+    , event_name : String
+    , image : String
+    , entrants : Maybe Int
+    , start_time : Int
+    }
+
+
+eventDecoder : D.Decoder TournamentEvent
+eventDecoder =
+    D.map6 TournamentEvent
+        (D.field "slug" D.string)
+        (D.field "tournament_name" D.string)
+        (D.field "event_name" D.string)
+        (D.field "image" D.string)
+        (D.field "entrants" (D.nullable D.int))
+        (D.field "start_time" D.int)
+
+
+toUtcString : Time.Posix -> String
+toUtcString time =
+    String.fromInt (toHour utc time)
+        ++ ":"
+        ++ String.fromInt (toMinute utc time)
+        ++ ":"
+        ++ String.fromInt (toSecond utc time)
+        ++ " (UTC)"
+
+
 tournamentDiv : TournamentEvent -> Html Msg
 tournamentDiv event =
     div [ class "fl w-50 w-25-m w-20-l pa2" ]
         [ img [ src event.image, class "db link dim tc" ] []
         , dl [ class "m52 f6 lh-copy" ]
-            [ dd [ class "ml0 black truncate w-100" ] [ text event.name ]
-            , dd [ class "m10 gray truncate w-100" ] [ text (String.fromInt event.entrants) ]
-            , dd [ class "ml0 gray truncate w-100" ] [ text event.startTime ]
+            [ dd [ class "ml0 black truncate w-100" ] [ text event.event_name ]
+            , dd [ class "m10 gray truncate w-100" ] [ text (String.fromInt (withDefault 0 event.entrants)) ]
+            , dd [ class "ml0 gray truncate w-100" ] [ text (toUtcString (Time.millisToPosix event.start_time)) ]
             ]
         ]
 
 
 doc : Model -> List (Html Msg)
-doc (Init events) =
-    [ article []
-        [ code []
-            [ h2 [ class "f3 fw4 pa3 mv0" ] [ text "Upcoming Melee Netplay Tournaments" ]
-            , h3 [ class "f5 fw4 pa3 mv0" ] [ text "Last Updated <TODO: UPDATE>" ]
+doc model =
+    case model of
+        Loading ->
+            [ text "loading" ]
+
+        Failure ->
+            [ text "failed" ]
+
+        Success events ->
+            [ article []
+                [ code []
+                    [ h2 [ class "f3 fw4 pa3 mv0" ] [ text "Upcoming Melee Netplay Tournaments" ]
+                    , h3 [ class "f5 fw4 pa3 mv0" ] [ text "Last Updated <TODO: UPDATE>" ]
+                    ]
+                , div [ class "cf pa2" ] (List.map tournamentDiv events)
+                ]
             ]
-        , div [ class "cf pa2" ] (List.map tournamentDiv events)
-        ]
-    ]
 
 
 view : Model -> Document Msg
